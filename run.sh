0f281e5bda509c3497fda4583f15c8e90c46173f
Mihai Csaky <mihai.csaky@gmail.com>
2015-10-07 13:29:18 +0300

    opengts

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #create config from jinja templates
-j2  /usr/local/config/build.properties.j2 /usr/local/config/build.properties.init > $GTS_HOME/build.properties
+j2  /usr/local/config/build.properties.j2 /usr/local/config/build.properties.ini > $GTS_HOME/build.properties
 
 
 cd $GTS_HOME; ant all
