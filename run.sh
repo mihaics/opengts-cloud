906a768864d3a83f9ffd678b440db248505a24f1
Mihai Csaky <mihai.csaky@gmail.com>
2015-10-08 16:27:22 +0300

    config

--- run.sh
+++ run.sh
@@ -2,10 +2,12 @@
 #create config from jinja templates
 # environment COMPILE_DEBUG=false, COMPILE_OPTIMIZE=true
 j2  $GTS_HOME/build.properties.j2 > $GTS_HOME/build.properties
+j2  $GTS_HOME/config.conf.j2 > $GTS_HOME/config.conf
 
 
 
 cd $GTS_HOME; ant all
 cp $GTS_HOME/build/*.war $CATALINA_HOME/webapps/
 #$CATALINA_HOME/bin/catalina.sh run
+/bin/bash
 
