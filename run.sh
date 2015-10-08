e1232aaeb313032d911c7923e232f693dfc4a12d
Mihai Csaky <mihai.csaky@gmail.com>
2015-10-08 12:51:57 +0300

    cleanup

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 #create config from jinja templates
-j2  /usr/local/opengts-cloud-master/config/build.properties.j2 /usr/local/opengts-cloud-master/config/build.properties.ini > $GTS_HOME/build.properties
+# environment COMPILE_DEBUG=false, COMPILE_OPTIMIZE=true
+j2  $GTS_HOME/build.properties.j2 > $GTS_HOME/build.properties
+
 
 
 cd $GTS_HOME; ant all
 cp $GTS_HOME/build/*.war $CATALINA_HOME/webapps/
-$CATALINA_HOME/bin/catalina.sh run
+#$CATALINA_HOME/bin/catalina.sh run
 
