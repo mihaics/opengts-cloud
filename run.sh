fdc18b2a56a68ed5c36888033286060a094268cb
Mihai Csaky <mihai.csaky@gmail.com>
2015-10-09 12:50:42 +0300

    jq

--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 #create config from jinja templates
 # environment COMPILE_DEBUG=false, COMPILE_OPTIMIZE=true
+# get environment from etcd
+# OPENGTS_CLIENT_ID and ETCD_SRV_ADDR should be initialized
+export MYSQL_DBNAME=$(curl -L http://$ETCD_SRV_ADDR:2379/v2/keys/opengts/$OPENGTS_CLIENT_ID/MYSQL_DBNAME | jq '.node.value' | sed 's/\"//g'  )
+export MYSQL_DBUSER=$(curl -L http://$ETCD_SRV_ADDR:2379/v2/keys/opengts/$OPENGTS_CLIENT_ID/MYSQL_DBUSER | jq '.node.value' | sed 's/\"//g'  )
+export MYSQL_DBPASSWORD=$(curl -L http://$ETCD_SRV_ADDR:2379/v2/keys/opengts/$OPENGTS_CLIENT_ID/MYSQL_DBPASSWORD | jq '.node.value' | sed 's/\"//g'  )
+
+
 j2  $GTS_HOME/build.properties.j2 > $GTS_HOME/build.properties
 j2  $GTS_HOME/config.conf.j2 > $GTS_HOME/config.conf
 
