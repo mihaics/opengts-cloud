8f27fb79f4523cac44201f284fe069afdf9f0129
Mihai Csaky <mihai.csaky@gmail.com>
2015-10-12 14:24:00 +0300

    kube

--- new_account.sh
+++ new_account.sh
@@ -6,3 +6,6 @@ etcdctl set /opengts/$1/SYSADMIN_PASSWORD $( < /dev/urandom tr -dc _A-Z-a-z-0-9 
 
 
 
+cp opengts-app.yml.j2 opengts-app.template
+sed -i "s/demo/$1/g" opengts-app-$1.yml
+kubectl create -f opengts-app-$1.yml
