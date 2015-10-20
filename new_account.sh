3b705873089153a8950b13ea99c507e560a06e36
Mihai Csaky <mihai.csaky@gmail.com>
2015-10-20 11:06:33 +0300

    srv

--- new_account.sh
+++ new_account.sh
@@ -3,6 +3,7 @@ etcdctl set /opengts/$1/MYSQL_DBNAME $1
 etcdctl set /opengts/$1/MYSQL_DBUSER $1
 etcdctl set /opengts/$1/MYSQL_DBPASSWORD $( < /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${2:-10};echo;)
 etcdctl set /opengts/$1/SYSADMIN_PASSWORD $( < /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${2:-10};echo;)
+etcdctl set /opengts/$1/CREATE_DATABASE true
 
 
 
--- run.sh
+++ run.sh
@@ -11,6 +11,9 @@ export MYSQL_DBNAME=$(curl -L http://$ETCD_SRV_ADDR:2379/v2/keys/opengts/$OPENGT
 export MYSQL_DBUSER=$(curl -L http://$ETCD_SRV_ADDR:2379/v2/keys/opengts/$OPENGTS_CLIENT_ID/MYSQL_DBUSER | jq '.node.value' | sed 's/\"//g'  )
 export MYSQL_DBPASSWORD=$(curl -L http://$ETCD_SRV_ADDR:2379/v2/keys/opengts/$OPENGTS_CLIENT_ID/MYSQL_DBPASSWORD | jq '.node.value' | sed 's/\"//g'  )
 export SYSADMIN_PASSWORD=$(curl -L http://$ETCD_SRV_ADDR:2379/v2/keys/opengts/$OPENGTS_CLIENT_ID/SYSADMIN_PASSWORD | jq '.node.value' | sed 's/\"//g'  )
+export MYSQL_ENV_MYSQL_ROOT_PASSWORD=$(curl -L http://$ETCD_SRV_ADDR:2379/v2/keys/opengts/database/mysql/MYSQL_ROOT_PASSWORD | jq '.node.value' | sed 's/\"//g'  )
+export CREATE_DATABASE=$(curl -L http://$ETCD_SRV_ADDR:2379/v2/keys/opengts/$OPENGTS_CLIENT_ID/CREATE_DATABASE | jq '.node.value' | sed 's/\"//g'  )
+
 
 j2  $GTS_HOME/build.properties.j2 > $GTS_HOME/build.properties
 j2  $GTS_HOME/config.conf.j2 > $GTS_HOME/config.conf
@@ -22,9 +25,14 @@ cp $GTS_HOME/build/*.war $CATALINA_HOME/webapps/
 
 
 #init db if needed
-$GTS_HOME/bin/initdb.pl --rootPass=$MYSQL_ENV_MYSQL_ROOT_PASSWORD
+if [$CREATE_DATABASE = "true" ]
+  $GTS_HOME/bin/initdb.pl --rootPass=$MYSQL_ENV_MYSQL_ROOT_PASSWORD;
+
 # create sysadmin account
-$GTS_HOME/bin/admin.pl Account -account=sysadmin -pass=$SYSADMIN_PASSWORD -create
+  $GTS_HOME/bin/admin.pl Account -account=sysadmin -pass=$SYSADMIN_PASSWORD -create;
+  curl -X PUT http://$ETCD_SRV_ADDR:2379/v2/keys/opengts/$OPENGTS_CLIENT_ID/CREATE_DATABASE -d value="false";
+
+fi
 
 
 $CATALINA_HOME/bin/catalina.sh run
